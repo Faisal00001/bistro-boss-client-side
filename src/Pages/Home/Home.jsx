4f0193739573ea3579a7cf34184783fbe19ebada
Faisal00001 <faisalosman798@gmail.com>
2024-03-28 11:04:35 +0600

    Implement parallex effect in menu page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,3 @@
-import { Helmet } from "react-helmet-async";
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Featured from "./Featured/Featured";
@@ -9,9 +8,7 @@ import Testimonials from "./Testimonials/Testimonials";
 const Home = () => {
     return (
         <div>
-            <Helmet>
-                <title>Bistro Boss | Menu</title>
-            </Helmet>
+
             <Banner></Banner>
             <Category></Category>
             <PopularMenu></PopularMenu>
--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,12 +1,12 @@
-import { Helmet } from 'react-helmet-async';
-
+import MenuImg from "../.../../../assets/menu/menu-bg.png";
+import Cover from '../../Shared/Cover/Cover';
+import PopularMenu from "../Home/PopularMenu/PopularMenu";
 const Menu = () => {
     return (
         <div>
-            <Helmet>
-                <title>Bistro Boss | Menu</title>
-            </Helmet>
+            <Cover img={MenuImg} title={'Our Menu'}></Cover>
             <h3 className="text-2xl">Welcome to menu page</h3>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
