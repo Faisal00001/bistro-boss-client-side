25f3833775d7781acc47571440b9cef65f32d736
Faisal00001 <faisalosman798@gmail.com>
2024-03-28 12:22:46 +0600

    Use custom hook to load the menu item

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,12 +1,11 @@
 import MenuImg from "../.../../../assets/menu/menu-bg.png";
 import Cover from '../../Shared/Cover/Cover';
-import PopularMenu from "../Home/PopularMenu/PopularMenu";
 const Menu = () => {
     return (
         <div>
             <Cover img={MenuImg} title={'Our Menu'}></Cover>
             <h3 className="text-2xl">Welcome to menu page</h3>
-            <PopularMenu></PopularMenu>
+
         </div>
     );
 };
